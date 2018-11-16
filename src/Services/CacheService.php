d9653656b0ecb8054b8e8086a92e69b910dc4939
= <ad.adamdavid72@gmail.com>
2018-11-16 15:09:18 +0500

    generalised CacheService class to be able to do any cache related task

--- src/Services/CacheService.php
+++ src/Services/CacheService.php
@@ -7,103 +7,30 @@ use Illuminate\Support\Facades\Cache;
 
 class CacheService
 {
-    protected $prefix;
-    
-    protected $chat_id;
-    
-    protected $user_id;
-    
-    public function __construct($chat_id, $user_id)
+    public function __construct()
     {
-        $this->prefix = ConfigService::getConversationCacheKey();
-        $this->chat_id = $chat_id;
-        $this->user_id = $user_id;
+        $this->removeExpiredMystConversations();
     }
     
-    protected function getMystCache()
+    public function getMystConversationsCache()
     {
-        return Cache::get($this->prefix);
+        return Cache::get(ConfigService::getConversationCacheKey());
     }
     
-    public function getCache()
+    public function saveMystConversationsCache(array $data)
     {
-        return $this->getMystCache()[$this->chat_id][$this->user_id] ?? null;
+        Cache::forever(ConfigService::getConversationCacheKey(), $data);
     }
     
-    public function getName()
+    protected function removeExpiredMystConversations()
     {
-        return $this->cache['name'] ?? null;
-    }
-    
-    public function getCurrentStep()
-    {
-        return $this->cache['step'] ?? null;
-    }
-    
-    public function getSteps()
-    {
-        return $this->cache['steps'] ?? null;
-    }
-    
-    public function getStep($step)
-    {
-        return $this->cache['steps'][$step] ?? null;
-    }
-    
-    public function expiresAt()
-    {
-        return $this->cache['expires_at'] ?? null;
-    }
-    
-    public function forward($update, $message_id)
-    {
-        if (!$this->getCache()) return false;
-        
-        $this->getCache()['step'] = $this->getCache()['step'] + 1;
-        $this->getCache()['steps'][] = $update;
-        $this->getCache()['reply_msg_id'] = $message_id;
-        
-        return $this->saveCache();
-    }
-    
-    public function init($name)
-    {
-        if ($this->getCache()) return false;
-        
-        $convo = [
-            'name'          => $name,
-            'step'          => 0,
-            'steps'         => [],
-            'reply_msg_id'  => null,
-            'expires_at'    => now()->addMinutes(60)
-        ];
-        
-        $this->getMystCache()[$this->chat_id][$this->user_id] = $convo;
-        return $this->saveCache();
-    }
-    
-    public function destroy()
-    {
-        unset($this->getMystCache()[$this->chat_id][$this->user_id]);
-        return $this->saveCache();
-    }
-    
-    protected function saveCache()
-    {
-        $this->getMystCache()[$this->chat_id][$this->user_id] = $this->getCache();
-        Cache::forever($this->prefix, $this->getMystCache());
-        
-        return true;
-    }
-    
-    protected function removeExpired()
-    {
-        foreach ($this->getMystCache() as $chat_id => $chat) {
+        $myst_conversations = $this->getMystConversationsCache();
+        foreach ($myst_conversations as $chat_id => $chat) {
             foreach ($chat as $user_id => $user) {
-                if (Carbon::parse($user['expires_at'])->greaterThanOrEqualTo(Carbon::now())) unset($this->getMystCache()[$chat_id][$user_id]);
+                if (Carbon::parse($user['expires_at'])->lessThanOrEqualTo(Carbon::now())) unset($myst_conversations[$chat_id][$user_id]);
             }
-            if (count($chat) < 1) unset($this->getMystCache()[$chat_id]);
+            if (count($chat) < 1) unset($myst_conversations[$chat_id]);
         }
-        return $this->saveCache();
+        $this->saveMystConversationsCache($myst_conversations);
     }
 }
