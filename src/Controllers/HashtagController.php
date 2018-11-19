9d77564c3d3d8e8336c57828063ca9b9fb523d0e
= <ad.adamdavid72@gmail.com>
2018-11-19 12:43:25 +0500

    forcing hashtags to be case insensitive (same goes with mentions)

--- src/Controllers/HashtagController.php
+++ src/Controllers/HashtagController.php
@@ -4,5 +4,8 @@ namespace Blaze\Myst\Controllers;
 
 abstract class HashtagController extends EntitiesController
 {
-
+    public function isCaseSensitive(): bool
+    {
+        return false;   //forcing hashtags to be case insensitive
+    }
 }
