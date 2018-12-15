8f52441921c85566db8f3b801edd0a704b0fe577
= <ad.adamdavid72@gmail.com>
2018-12-15 18:34:33 +0500

    added in position method

--- src/Api/Objects/Entity.php
+++ src/Api/Objects/Entity.php
@@ -24,4 +24,35 @@ class Entity extends ApiObject
     {
         return substr($text, $this->getOffset(), $this->getLength());
     }
+    
+    public function inPosition($text, $position)
+    {
+        switch ($position) {
+            case 'any':
+            case '*':
+                return true;
+                break;
+            case 'start':
+            case 0:
+                if ($this->getOffset() == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            case 'end':
+                if (strlen($text) == ($this->getOffset() + $this->getLength())) {
+                    return true;
+                } else {
+                    return false;
+                }
+                break;
+            default:
+                if ((int)$position == $this->getOffset()) {
+                    return true;
+                } else {
+                    return false;
+                }
+        }
+    }
 }
